e94ccc07f7e822c4d318c6a0181b8aba9e23bad2
xuzx1210 <xuzx1210@gmail.com>
2023-08-25 17:12:23 +0800

    list specific number of links

--- server.py
+++ server.py
@@ -13,8 +13,11 @@ from pytube import YouTube
 from pytube.contrib.playlist import Playlist
 
 client = commands.Bot(command_prefix=">", intents=Intents.all(), activity=Game(name=">help"), status=Status.online)
-playQueues = {}  # dict[guildId, list[url]]
-nowPlaying = {}  # dict[guildId, url]
+
+playQueues: dict[int, list[str]] = {}  # dict[guildId, list[url]]
+nowPlaying: dict[int, str] = {}  # dict[guildId, url]
+
+MAX_LENGTH: int = 90
 
 
 @client.event
@@ -46,7 +49,7 @@ async def join(ctx: Context):
 async def leave(ctx: Context):
     guild: Guild = ctx.guild
     voiceClient: VoiceClient = get(client.voice_clients, guild=guild)
-    id = guild.id
+    id: int = guild.id
 
     if voiceClient == None:
         await ctx.send(content="機器人尚未連線至任何語音頻道")
@@ -63,8 +66,8 @@ async def leave(ctx: Context):
 
 
 def playNext(guild: Guild):
-    id = guild.id
-    filename = "music/" + str(id) + ".mp4"
+    id: int = guild.id
+    filename: str = "music/" + str(id) + ".mp4"
     if exists(path=filename):
         remove(path=filename)
 
@@ -99,14 +102,18 @@ async def play(ctx: Context, url: str):
     if voiceClient.channel != voiceState.channel:
         await ctx.send(content="您與機器人處於不同語音頻道\n請先使用 'leave' 指令再使用 'join' 指令\n或移動至機器人所在之語音頻道")
 
-    urlType: str = url.split('?')[0].split('/')[-1]
+    urlType: str = ""
+    try:
+        urlType = url.split('?')[0].split('/')[-1]
+    except Exception:
+        await ctx.send(content="連結無效")
+        return
     if urlType == "watch":
         playQueues[guild.id].append(url)
     elif urlType == "playlist":
-        playlist = Playlist(url=url)
-        playQueues[guild.id] += playlist.video_urls
+        playQueues[guild.id] += Playlist(url=url).video_urls
     else:
-        await ctx.send("非YouTube影片或清單連結")
+        await ctx.send(content="非YouTube影片或清單連結")
         return
 
     if voiceClient.is_playing():
@@ -120,14 +127,14 @@ async def show(ctx: Context):
     guild: Guild = ctx.guild
     voiceClient: VoiceClient = get(client.voice_clients, guild=guild)
     if not voiceClient.is_playing():
-        await ctx.send("無播放中音樂")
+        await ctx.send(content="無播放中音樂")
         return
-    await ctx.send(nowPlaying[guild.id])
+    await ctx.send(content=nowPlaying[guild.id])
 
 
-@client.command(help="顯示音樂清單")
-async def list(ctx: Context):
-    id = ctx.guild.id
+@client.command(help=f"顯示音樂清單，最多顯示{MAX_LENGTH}個連結")
+async def list(ctx: Context, nums: str = ""):
+    id: int = ctx.guild.id
 
     if id not in playQueues:
         await ctx.send(content="無音樂清單")
@@ -135,13 +142,31 @@ async def list(ctx: Context):
     if len(playQueues[id]) == 0:
         await ctx.send(content="清單中無音樂")
         return
-    if 90 < len(playQueues[id]):
-        await ctx.send(content=f"共有{len(playQueues[id])}首音樂，無法列出所有連結")
+
+    if nums == "":
+        if MAX_LENGTH < len(playQueues[id]):
+            await ctx.send(content=f"清單中有{len(playQueues[id])}首音樂，無法列出所有連結")
+        else:
+            result = ""
+            for url in playQueues[id]:
+                result += url + '\n'
+            await ctx.send(content=result)
         return
 
-    result = ""
-    for url in playQueues[id]:
-        result += url + '\n'
+    length: int = 0
+    try:
+        length = int(nums)
+    except ValueError:
+        await ctx.send(content="請輸入數字")
+        return
+    if length <= 0:
+        await ctx.send(content="請輸入正整數")
+        return
+    length = min(length, MAX_LENGTH, len(playQueues[id]))
+
+    result: str = ""
+    for i in range(length):
+        result += playQueues[id][i] + '\n'
     await ctx.send(content=result)
 
 
